4c0a62b388a783a5bb903f7cd278bd0103712e35
GargiDahake04 <164405858+GargiDahake04@users.noreply.github.com>
2024-05-11 11:57:56 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 24962993
+let accountEmail = "Gargid24@gmail.com"
+var accountPassword = "9693"
+accountCity = "Bhandara"
+
+//accountId = 29
+accountEmail = "Srk04@gmail.com"
+accountPassword = "9796"
+accountCity = "Balaghat"
+
+/* Note:- Prefer not to use var
+         Because of issue in Block scope and Functional scope */
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use Strict "; //it always give newer verison of ur code//
+
+//alert(3+3)   we are using Node js not a Browser//
+
+
+let name = "Gargi"
+let age = 22
+let isLoggedIn = False
+
+
+// number => 2 to power 53
+// bigInt =>
+// boolean => True/False 
+// Null => Standalone Value
+//undefined => 
+//symbol => Unique 
+
+//Object =>
+
+
+    console.log(typeof null);  //Object
+    console.log(typeof undefined);  //Undefined
