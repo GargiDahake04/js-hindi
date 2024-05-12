bdf16cf9389d871067c13d4db6569d6c9e14e815
GargiDahake04 <164405858+GargiDahake04@users.noreply.github.com>
2024-05-12 12:56:13 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let value = Number(score)
-console.log(typeof value);
-console.log(value);
+//let value = Number(score)
+//console.log(typeof value);
+//console.log(value);
 
 
 //"33" => 33
@@ -14,13 +14,52 @@ console.log(value);
 //True => 1 ; False => 0
 //Undefined => NaN
 
-let isLoggedIn= 1
+let isLoggedIn;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => True; 0 => False
 // "" => False
 //"Gargi" => True
+//"Undefined" => False
+
+//***********************************************Operations****************************************************
+
+ let value = 4
+
+ let negValue= -value
+
+ console.log(negValue);
+
+ let str1= "Hieee"
+ let str2= " Gargi"
+ let str3= str1 + str2
+
+ console.log(str3);
+
+ console.log("1" + 2);
+ console.log(2 + "1");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + 2);
+
+ console.log(3 + 4 * 5);
+
+ console.log(+true);
+ console.log(+false);
+ console.log(true);
+ console.log(true + "Kallu");
+
+ console.log(+"");
+
+ let getValue= 100
+ ++getValue
+ console.log(getValue);
+
+let nr =1
+
+ console.log(nr++);
+ console.log(nr);
+ console.log(++nr);
