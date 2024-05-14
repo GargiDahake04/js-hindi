ae505a6d190b2ab893dcff5bf9aee5610f498884
GargiDahake04 <164405858+GargiDahake04@users.noreply.github.com>
2024-05-14 11:32:43 +0000

    hsgsh

---
+++ 01_basics/06_string.js
@@ -0,0 +1,22 @@
+const name = "Gargi"
+const repoCount = 24
+
+console.log(`Hieee my name is ${name} and my RepoCount are ${repoCount}`);
+
+
+let nickName = "Kalyani"
+
+console.log(nickName.indexOf('a'));
+console.log(nickName.charAt(4));
+console.log(nickName.substring(1, 5));
+console.log(nickName.slice(-2, 7));
+
+
+
+const game = new String('Mario')
+
+console.log(game.slice(-5, 3));
+
+const fox = " my name is Gargi-dahake"
+
+console.log(fox.split('-'));
