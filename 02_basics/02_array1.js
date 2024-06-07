95f600cab6095b2e0f2dc49741e36e5ddfc85821
GargiDahake04 <164405858+GargiDahake04@users.noreply.github.com>
2024-06-07 11:27:17 +0000

    ggg

--- 02_basics/02_array1.js
+++ 02_basics/02_array1.js
@@ -15,3 +15,22 @@ const allMembers= [...hyungLine, ...maknaeLine]
 console.log(allMembers);
 
 
+const num = [1, 2, 3, [4, 5, 5], 7, 6, [8, 9, [1, 2],7]]
+
+const realnum =num.flat(Infinity);
+
+console.log(realnum);
+
+
+
+console.log(Array.isArray("Gargi"));
+console.log(Array.from("Gargi"));
+console.log(Array.from({name: "Gargi"})); //Intresting
+
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+
+console.log(Array.of(score1, score2, score3));
